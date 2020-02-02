38de90c427b27826af5929937a20daf356e4ff84
yzakzero <49981614+yzakzero@users.noreply.github.com>
2020-02-02 23:03:02 +0800

    Update dd.py

--- dd.py
+++ dd.py
@@ -1,4 +1,4 @@
-curl 'https://oapi.dingtalk.com/robot/send?access_token=xxxxxxxx' \
+curl 'https://oapi.dingtalk.com/robot/send?access_token=350ef93014a35fd97f33c4849e7747153cf314ff52736165d66ab7a1759c3fff' \
    -H 'Content-Type: application/json' \
    -d '{"msgtype": "text", 
         "text": {
